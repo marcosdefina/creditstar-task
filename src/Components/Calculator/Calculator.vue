7810cbc876026b848bd0752c91345558bd82ed53
Marcos Defina <marcosdefina@gmail.com>
2019-07-10 01:46:38 +0300

    basic interest calculation

--- src/Components/Calculator/Calculator.vue
+++ src/Components/Calculator/Calculator.vue
@@ -13,21 +13,43 @@ export default {
       loanFormatter: v => `€${("" + v).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`,
       loan: 500,
       time: 7,
-      styleTest: "transform: translate(" + translate_bar + "px,-15px)",
       slider: {
         lineHeight: 10,
         processStyle: {
-          lineHeight: 10,
           backgroundColor: "#ffb700"
         }
-      }
+      },
+
+      pickedDay: new Date('02/07/2019'),
+      yearlyInterest: 0.25,
+      totalAmountToPay: this.TotalToPay(this.loan, this.time, this.pickedDay),
     };
   },
   methods: {
-    incrementSlide() {
-      this.translate_bar += 30;
-      if (this.translate_bar > 160) this.translate_bar = -160;
-    }
+    InterestConverter: function(totalInterest, discreteInterval){
+      return Math.pow((1+totalInterest),(1/discreteInterval));
+    },
+    TotalToPay: function(loan, months, pickedDay){
+      //loan * monthsInterst*months + loan * (|now - pickedDay| * dailyInterest)
+      var today = new Date();
+
+      var monthlyInterest = this.InterestConverter(this.yearlyInterest, 12);
+      
+      if(this.formatDate(today) != this.formatDate(pickedDay))
+        var dailyInterest = this.InterestConverter(this.yearlyInterest, 365.25);
+      
+      return loan * (Math.pow(monthlyInterest, months))// + (Math.pow(dailyInterest, this.dayModule(today, pickedDay))));
+    },
+    formatDate: function(date = new Date()){
+      return (date.getMonth()+1)+'/'+date.getDate()+'/'+date.getFullYear();
+    },
+    dayModule: function(date1 = new Date(), date2  = new Date('01/01/2001')){
+      const diffTime = Math.abs(date2.getTime() - date1.getTime());
+      return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+      //We could to discretize more and more until get fair.
+      //But will never be fair until the discretization takes the limit to a continuous interval.
+      //So lets round up.
+    },
   },
   components: {
     VueSlider,
@@ -35,7 +57,6 @@ export default {
   }
 };
 
-var translate_bar = -30;
 </script>
 <template src="./Calculator.html"/>
 <style src="./Calculator.css"/>
